9ef8f1b9519294a271d6bc01a6c926a52a1200f5
Samuel Batistela <samuel.batistela12@gmail.com>
2022-01-07 10:47:23 -0300

    desafio finalizado

--- src/index.js
+++ src/index.js
@@ -10,19 +10,72 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "user not found" })
+  }
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if ((!user.pro && user.todos.length < 10) || user.pro) {
+    return next()
+  } else {
+    return response.status(403).json({
+      error:
+        "Your subscription is FREE and you ran out of todos :(. Go PRO and enjoy"
+    });
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username)
+  const validId = validate(id);
+
+  if(!user) {
+    return response.status(404).json({error: "Todo user not found"})
+  }
+
+  if(!validId) {
+    return response.status(404).json({error: "Todo id is not valid"})
+  }
+
+  const todo = user.todo.find((todo) => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({error: "Todo not found"})
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  request.user = user;
+
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
